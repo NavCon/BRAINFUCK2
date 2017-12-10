052682240874a402346d13350c0638e1c7666128
_NavCon <NavCon@users.noreply.github.com>
2017-12-10 22:55:54 +0100

    Parentheses-Error fixed + Visual Interpreting Mode
    Fixed my Error that the Interpreter was'nt counting open Parentheses and thus only searched for the last opening one. -> Nesting was not possible.
    
    Furthermore I added the "Visual Interpreting Mode", which is a Runtime Visualisation of the interpreter working.
    
    Thanks to Skurmedel (https://github.com/skurmedel) for the colours!

--- main.cpp
+++ main.cpp
@@ -1,14 +1,50 @@
-// AUTHOR: navcon
-// DATE  : 12/10/2017
+// AUTHOR : navcon
+// DATE   : 12/10/2017
+// LICENSE: GPLv3.0
+
+// THANKS TO Skurmedel (https://github.com/skurmedel) for the colours!
+
 
 #include <stdio.h>
 #include <iostream>
 #include <Windows.h>
 #include <conio.h>
 #include <string>
+#include <iomanip>
 using namespace std;
 
+
+
+
+enum colour { DARKBLUE = 1, DARKGREEN, DARKTEAL, DARKRED, DARKPINK, DARKYELLOW, GRAY, DARKGRAY, BLUE, GREEN, TEAL, RED, PINK, YELLOW, WHITE };
+
+struct setcolour
+{
+	colour _c;
+	HANDLE _console_handle;
+
+
+	setcolour(colour c, HANDLE console_handle)
+		: _c(c), _console_handle(0)
+	{
+		_console_handle = console_handle;
+	}
+};
+
+// We could use a template here, making it more generic. Wide streams won't
+// work with this version.
+basic_ostream<char> &operator<<(basic_ostream<char> &s, const setcolour &ref)
+{
+	SetConsoleTextAttribute(ref._console_handle, ref._c);
+	return s;
+}
+
+
+
 int main() {
+
+	HANDLE chandle = GetStdHandle(STD_OUTPUT_HANDLE);
+
 	while (true) {
 
 		int pcounter = 0;
@@ -17,11 +53,19 @@ int main() {
 		int pointer = 0;
 		byte outarray[100];
 
-		string code = "";
+		string code = " ";
+
+		int open_parentheses = 0;
 
 		bool run_flag = true;
+		bool step_mode = true;
+		bool fast_mode = false;
+		bool very_fast_mode = false;
 
-		char input = '0';
+		string input = "";
+		int input_int = 0;
+
+		string output = "";
 
 		cout << "--BRAINFUCK2 INTERPRETER--" << endl << endl << endl;
 		cout << "SUPPORTED COMMANDS:" << endl;
@@ -49,10 +93,21 @@ int main() {
 
 		//MAIN LOOP
 		while (code[pcounter] != '\0' && pcounter >= 0 && run_flag) {
+			char instr = code[pcounter]; //DEBUG
+			//printf("pcounter: %5i   pointer: %3i   instruction: %c   value:  before: %3i   ", pcounter, pointer, code[pcounter], outarray[pointer]);
 
-			printf("pcounter: %5i   pointer: %3i   instruction: %c   value:  before: %3i   ", pcounter, pointer, code[pcounter], outarray[pointer]);
+			system("cls");
+
+			// OUTPUT CODE AT TOP
+			for (int j = 0; j < code.length(); j++)
+			{
+				if (pcounter == j) cout << setcolour(RED, chandle);
+				else cout << setcolour(GRAY, chandle);
+				printf("%c", code[j]);
+			}
 
 
+			// MAIN INTERPRETING PART
 			switch (code[pcounter]) {
 			case 'n':
 			case '>':
@@ -79,16 +134,27 @@ int main() {
 			case 'o':
 			case '.':
 				pcounter++;
-				cout << outarray[pointer];
+				output += to_string(outarray[pointer]) + " ";
 				break;
 
 			case 'i':
 			case ',':
 				pcounter++;
-				cout << endl << "Value of " << pointer << ": ";
-				input = _getch();
+				input_int = -1;
+				while (input_int == -1) {
+					cout << endl << "Value of " << pointer << ": ";
+					cin >> input;
+
+					try {
+						input_int = stoi(input);
+					}
+					catch (invalid_argument) {
+						input_int = -1;
+					}
+				}
+
 				cout << input << endl;
-				outarray[pointer] = input;
+				outarray[pointer] = (byte)input_int;
 				break;
 
 			case '[':
@@ -102,8 +168,12 @@ int main() {
 				}
 				else {
 					nextpcounter = pcounter;
-					while (code[nextpcounter] != '[') {
+					open_parentheses = 1;
+					while (open_parentheses) {
 						nextpcounter--;
+						instr = code[nextpcounter];
+						if (code[nextpcounter] == ')' || code[nextpcounter] == ']') open_parentheses++;
+						else if (code[nextpcounter] == '[') open_parentheses--;
 					}
 					pcounter = nextpcounter;
 				}
@@ -116,8 +186,12 @@ int main() {
 				}
 				else {
 					nextpcounter = pcounter;
-					while (code[nextpcounter] != '[') {
+					open_parentheses = 1;
+					while (open_parentheses) {
 						nextpcounter--;
+						instr = code[nextpcounter];
+						if (code[nextpcounter] == ')' || code[nextpcounter] == ']') open_parentheses++;
+						else if (code[nextpcounter] == '[') open_parentheses--;
 					}
 					pcounter = nextpcounter;
 				}
@@ -147,10 +221,42 @@ int main() {
 				break;
 			}
 
-			printf("after: %3i\n", outarray[pointer]);
+			// SHOW
+			cout << endl << endl;
+
+			for (int i = 0; i < sizeof(outarray); i++)
+			{
+				if (i % 10 == 0) cout << endl;
+				if (pointer == i) cout << setcolour(RED, chandle);
+				else cout <<  setcolour(GRAY, chandle);
+				printf(" %03i", outarray[i]);
+			}
+			
+			cout << endl << endl << "  " + output << endl;
+
+			if(step_mode) cout << endl << endl << "Press any key to continue... or (r)unmode / (f)astmode / (v)eryfastmode";
+
+			if (!step_mode && !very_fast_mode) Sleep(fast_mode?150:500);
+			else if (very_fast_mode) Sleep(25);
+			else {
+				char c1 = _getch();
+				if (c1 == 'r') step_mode = false;
+				else if (c1 == 'f') {
+					step_mode = false;
+					fast_mode = true;
+				}
+				else if (c1 == 'v') {
+					step_mode = false;
+					very_fast_mode = true;
+				}
+			}
 		}
 
 		cout << endl;
+
+		system("cls");
+
+		cout << code << endl << endl;
 
 		for (int i = 0; i < sizeof(outarray); i++)
 		{
@@ -158,12 +264,19 @@ int main() {
 			printf(" %03i", outarray[i]);
 		}
 
-		cout << endl;
+		cout << endl << endl << setcolour(RED, chandle);
+		for (int i = 0; i < 40 - output.length() + 1; i++) cout << " ";
+		cout << output;
+		cout << setcolour(GRAY, chandle) << endl;
+
+		cout << endl << endl;
 
 
 		cout << "Run again? (Y/n):";
 		char c = _getch();
 		if (c == 'n' || c == 'N') return 0;
 		else system("cls");
+
+
 	}
 }
